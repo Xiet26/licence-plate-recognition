04e91b9f60f8d0583d6be387287f8d4185a934e6
Nguyen Ngoc Phuoc <phuoc0191vn@gmail.com>
2019-11-17 17:43:24 +0700

    recognize character - SVM

--- core/main.go
+++ core/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"git.cyradar.com/phuocnn/licence-plate-recognition/core/utilities"
+	libSvm "github.com/ewalker544/libsvm-go"
 	"gocv.io/x/gocv"
 	"golang.org/x/image/colornames"
 	"image"
 	"sort"
 )
 
-const FOLDER_PART = `` //image folder
+const FOLDER_PART = `/home/phuoc/work-go/src/git.cyradar.com/phuocnn/licence-plate-recognition/licenceplatesimage/` //image folder
 
 var (
 	lengthOfLicencePlate = 9
-	fileName             = `` //image name
+	fileName             = "DL7CQ1939.jpg" //image name
+	dataMap              = utilities.DataMap()
 )
 
 func main() {
@@ -28,20 +31,27 @@ func main() {
 	//Pre-Processing
 	grayImg := gocv.NewMat()
 	gocv.CvtColor(img, &grayImg, gocv.ColorBGRToGray)
+
 	bilateral := gocv.NewMat()
 	gocv.BilateralFilter(grayImg, &bilateral, 9, 75, 75)
+
 	equal := gocv.NewMat()
 	gocv.EqualizeHist(bilateral, &equal)
+
 	kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Point{5, 5})
 	defer kernel.Close()
 	morph := gocv.NewMat()
 	gocv.MorphologyExWithParams(equal, &morph, gocv.MorphOpen, kernel, 20, gocv.BorderDefault)
+
 	sub := gocv.NewMat()
 	gocv.Subtract(equal, morph, &sub)
+
 	thresh := gocv.NewMat()
 	gocv.Threshold(sub, &thresh, 0, 255, gocv.ThresholdOtsu)
+
 	canny := gocv.NewMat()
 	gocv.Canny(thresh, &canny, 250, 255)
+
 	kernel = gocv.GetStructuringElement(gocv.MorphRect, image.Point{3, 3})
 	dilate := gocv.NewMat()
 	gocv.Dilate(canny, &dilate, kernel)
@@ -49,20 +59,11 @@ func main() {
 	//Find licence plate
 	//Find contour
 	contours := gocv.FindContours(dilate, gocv.RetrievalTree, gocv.ChainApproxSimple)
-	//Sort contour(Area)
-	contoursArea := make(map[float64][]image.Point)
-	var keys []float64
-	for _, v := range contours {
-		key := gocv.ContourArea(v)
-		contoursArea[key] = v
-		keys = append(keys, key)
-	}
-	sort.Float64s(keys)
 
-	contours = [][]image.Point{}
-	for i := len(keys); i > 0; i-- {
-		contours = append(contours, contoursArea[keys[i-1 ]])
-	}
+	//Sort contour(Area)
+	sort.Slice(contours, func(i, j int) bool {
+		return gocv.ContourArea(contours[i]) > gocv.ContourArea(contours[j])
+	})
 
 	//Find and border licence plate
 	var licencePlateBound image.Rectangle
@@ -82,21 +83,26 @@ func main() {
 	//Pre-Processing
 	licencePlateGray := gocv.NewMat()
 	gocv.CvtColor(licencePlate, &licencePlateGray, gocv.ColorBGRToGray)
+
 	licencePlateBlur := gocv.NewMat()
 	gocv.GaussianBlur(licencePlateGray, &licencePlateBlur, image.Point{
 		X: 3,
 		Y: 3,
 	}, 0, 0, gocv.BorderDefault)
-	licencePlateThre := gocv.NewMat()
-	gocv.Threshold(licencePlateBlur, &licencePlateThre, 120, 255, gocv.ThresholdBinaryInv)
+
+	licencePlateThresh := gocv.NewMat()
+	gocv.Threshold(licencePlateBlur, &licencePlateThresh, 120, 255, gocv.ThresholdBinaryInv)
+
 	kernel = gocv.GetStructuringElement(gocv.MorphRect, image.Point{
 		X: 3,
 		Y: 3,
 	})
 	licencePlateMor := gocv.NewMat()
-	gocv.MorphologyEx(licencePlateThre, &licencePlateMor, gocv.MorphDilate, kernel)
+	gocv.MorphologyEx(licencePlateThresh, &licencePlateMor, gocv.MorphDilate, kernel)
+
 	//Find Contour
 	contourNumbers := gocv.FindContours(licencePlateMor, gocv.RetrievalTree, gocv.ChainApproxSimple)
+
 	//Sort contour(area)
 	contourNumberAreas := make(map[float64][]image.Point)
 	var keyNumbers []float64
@@ -106,22 +112,70 @@ func main() {
 		keyNumbers = append(keyNumbers, key)
 	}
 	sort.Float64s(keyNumbers)
-	contourNumbers = [][]image.Point{}
+
+	// this here
 	//Find number in licence plate
+	contourNumbers = [][]image.Point{}
 	for i := len(keyNumbers); i > 0; i-- {
 		if i > len(keyNumbers)-lengthOfLicencePlate-1 && i < len(keyNumbers)-1 {
 			contourNumbers = append(contourNumbers, contourNumberAreas[keyNumbers[i-1]])
 		}
 	}
+
+	characterImg := make([]gocv.Mat, 0)
 	//Border number in licence plate
 	for _, v := range contourNumbers {
 		rect := gocv.BoundingRect(v)
 		gocv.Rectangle(&licencePlate, rect, colornames.Red, 2)
+		characterImg = append(characterImg, licencePlate.Region(rect))
 	}
+
+	imgForRecognize := make(map[int]gocv.Mat)
+	for k, v := range characterImg {
+		tmp := gocv.NewMat()
+		gocv.Resize(v, &tmp, image.Point{28, 28}, 0, 0, gocv.InterpolationNearestNeighbor)
+		imgForRecognize[k] = tmp
+	}
+
+	// load model
+	model := libSvm.NewModelFromFile("/home/phuoc/work-go/src/git.cyradar.com/phuocnn/licence-plate-recognition/train.svm")
+
+	for i := 0; i < len(imgForRecognize); i++ {
+		grayImg := gocv.NewMat()
+		gocv.CvtColor(imgForRecognize[i], &grayImg, gocv.ColorBGRToGray)
+
+		thresh := gocv.NewMat()
+		gocv.Threshold(grayImg, &thresh, 128, 255, gocv.ThresholdBinaryInv)
+
+		gocv.IMWrite(fmt.Sprintf("%d.png", i), thresh)
+
+		x := utilities.MatToMapIntFloat(thresh)
+
+		data := model.Predict(x)
+		fmt.Println(fmt.Sprintf("%d: %s", i, dataMap[data]))
+
+		grayImg.Close()
+		thresh.Close()
+	}
+
 	//Show result
-	window := gocv.NewWindow("hello")
+	//ShowImg(grayImg, "gray")
+	//ShowImg(bilateral, "bilateral")
+	//ShowImg(equal, "equal")
+	//ShowImg(morph, "morph")
+	//ShowImg(sub, "sub")
+	//ShowImg(thresh, "thresh")
+	//ShowImg(canny, "canny")
+	//ShowImg(dilate, "dilate")
+	ShowImg(img, "Result detect")
+}
+
+func ShowImg(img gocv.Mat, name string) {
+	window := gocv.NewWindow(name)
+	defer window.Close()
 	for {
-		window.IMShow(licencePlate)
+		window.ResizeWindow(800, 600)
+		window.IMShow(img)
 		if window.WaitKey(1) >= 0 {
 			break
 		}
